5cedfbb699a31e7565be43e1ce139d9ebe0e1552
Amanda Morgan <AmandaC0022@gmail.com>
2021-11-23 16:42:08 -0600

    navbar menu is built

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,47 +1,93 @@
-import * as React from 'react';
+import React, { useState, useEffect } from 'react';
 import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
-// import { theme } from '../Theme/Theme'; 
 import { styled } from '@mui/system';
 import Logo from '../Images/Checkmatelogofinal.png'; 
+import Avatar from '@mui/material/Avatar';
+import Divider from '@mui/material/Divider';
+import Drawer from '@mui/material/Drawer';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import Switch from '@mui/material/Switch';
+import MenuItem from '@mui/material/MenuItem';
+import Menu from '@mui/material/Menu';
 
-const MyToolbar = styled(Toolbar)(({ theme }) => ({
-    background: theme.palette.grey.main, 
+const MyLogo = styled("img")(() => ({
+  width: 300,  
 })); 
 
-const NavBtn = styled(Button)(({ theme }) => ({
-    '&:hover': {
-        background: theme.palette.grey.dark, 
-    } 
+const NavBtn = styled(Button)(({ theme }) => ({ 
+  '&:hover': {
+      background: theme.palette.grey.dark, 
+  } 
 })); 
 
+export default function MenuAppBar() {
+  const [auth, setAuth] = React.useState(true);
+  const [anchorEl, setAnchorEl] = React.useState(null);
 
-export default function ButtonAppBar(theme) {
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <MyToolbar>
+      <AppBar 
+        position="static" 
+        sx={{ backgroundColor: 'grey.main'}}
+      >
+        <Toolbar>
           <IconButton
             size="large"
             edge="start"
-            color='inherit'
+            color="inherit"
             aria-label="menu"
             sx={{ mr: 2 }}
+            aria-controls="menu-appbar"
+            aria-haspopup="true"
+            onClick={handleMenu}
           >
             <MenuIcon />
           </IconButton>
-          {/* <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Home
-          </Typography> */}
-          <img src={Logo} width="200"/>  
-          <NavBtn color="inherit">Login</NavBtn>
-          <NavBtn edge="end" color="inherit">Sign Up!</NavBtn>
-        </MyToolbar>
+          <Menu
+                id="menu-appbar"
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+              >
+                <MenuItem onClick={handleClose}>Login</MenuItem>
+                <MenuItem onClick={handleClose}>Register</MenuItem>
+                <MenuItem onClick={handleClose}>Profile</MenuItem>
+                <MenuItem onClick={handleClose}>How To Play</MenuItem>
+                <MenuItem onClick={handleClose}>Start A Game</MenuItem>
+                <MenuItem onClick={handleClose}>Logout</MenuItem>
+              </Menu>
+          <MyLogo src={Logo} /> 
+          {auth && (
+            <div>
+              <Avatar sx={{backgroundColor: "primary.main"}}>AM</Avatar>
+            </div>
+          )}
+        </Toolbar>
       </AppBar>
     </Box>
   );
