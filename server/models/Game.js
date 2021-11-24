91c6a73a870c18192c8430a95c9c811a61d6526c
Luke Poirrier <lukepoirrier5@gmail.com>
2021-11-23 21:28:36 -0500

    Create typeDefinitions for adding users, matches, teams, etc.

--- server/models/Game.js
+++ server/models/Game.js
@@ -14,6 +14,7 @@ const Match = require('./Match');
 // > - officialURL: String
 // > - rulesUrl: String
 // > - thumbnail: String
+
 // This is a subdocument schema, it won't become its own model but we'll use it as the schema for the User's `savedBooks` array in User.js
 const gameSchema = new Schema({
 	game_id: {
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -6,16 +6,41 @@ const typeDefs = gql`
     username: String!
     email: String
     bookCount: Int
-    savedBooks: [Book]
+    matches: [Match]
+    teams: [Team]
+    games: [Game]
+    wins: Int
+    losses: Int
+    avatar: String
   }
 
-  type Book {
-    bookId: ID!
-    authors: [String]
+  type Match {
+    _id: ID!
+    team1: Team!
+    team2: Team!
+    team1Score: Int
+    team2Score: Int
+    winner: Team
+    loser: Team
+    game_id: String
+  }
+
+  type Team {
+    _id: ID!
+    name: String!
+    members: [User]
+    games: [Game]
+    wins: Int
+    losses: Int
+  }    
+
+  type Game {
+    _id: ID!
     description: String
-    image: String
-    link: String
-    title: String!
+    game_id: String!
+    name: String
+    matches: [Match]
+    teams: [Team]
   }
 
   type Auth {
@@ -23,22 +48,31 @@ const typeDefs = gql`
     user: User
   }
 
-  input BookInput {
-    authors: [String]
-    description: String!
-    bookId: String!
-    image: String
-    link: String
-    title: String!
-  }
+  # input BookInput {
+  #   authors: [String]
+  #   description: String!
+  #   bookId: String!
+  #   image: String
+  #   link: String
+  #   title: String!
+  # }
 
   type Query {
     me: User
+    user(_id: ID!): User
+    users: [User]
+    game(_id: ID!): Game
+    games: [Game]
+    team(_id: ID!): Team
+    match(_id: ID!): Match
+    matches: [Match]
   }
 
   type Mutation {
     login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
+    addMatch(team1: String!, team2: String!, team1Score: Int, team2Score: Int, winner: String, loser: String, game_id: String): Match
+    addTeam(name: String!): Team
     saveBook(bookData: BookInput!): User
     removeBook(bookId: ID!): User
   }
