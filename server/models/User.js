b6a3c68bc7dbb9ae54e1760d8de800cbaecb016b
Luke Poirrier <lukepoirrier5@gmail.com>
2021-11-23 20:22:00 -0500

    Add imports to User model

--- server/models/User.js
+++ server/models/User.js
@@ -1,8 +1,10 @@
-const { Schema, model } = require('mongoose');
-const bcrypt = require('bcrypt');
+const { Schema, model } = require("mongoose");
+const bcrypt = require("bcrypt");
 
 // import schema from Book.js
-const bookSchema = require('./Book');
+const Match = require("./Match");
+const Team = require("./Team");
+const Game = require("./Game");
 
 const userSchema = new Schema(
   {
@@ -33,25 +35,25 @@ const userSchema = new Schema(
 );
 
 // hash user password
-userSchema.pre('save', async function (next) {
-  if (this.isNew || this.isModified('password')) {
-    const saltRounds = 10;
-    this.password = await bcrypt.hash(this.password, saltRounds);
-  }
+userSchema.pre("save", async function (next) {
+	if (this.isNew || this.isModified("password")) {
+		const saltRounds = 10;
+		this.password = await bcrypt.hash(this.password, saltRounds);
+	}
 
-  next();
+	next();
 });
 
 // custom method to compare and validate password for logging in
 userSchema.methods.isCorrectPassword = async function (password) {
-  return bcrypt.compare(password, this.password);
+	return bcrypt.compare(password, this.password);
 };
 
 // when we query a user, we'll also get another field called `bookCount` with the number of saved books we have
-userSchema.virtual('bookCount').get(function () {
-  return this.savedBooks.length;
-});
+// userSchema.virtual("bookCount").get(function () {
+// 	return this.savedBooks.length;
+// });
 
-const User = model('User', userSchema);
+const User = model("User", userSchema);
 
 module.exports = User;
