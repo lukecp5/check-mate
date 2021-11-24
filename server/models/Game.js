0ebef1b12ad2a3a9ebf7f55eedc04b7ffd8d5ec4
Luke Poirrier <lukepoirrier5@gmail.com>
2021-11-23 21:27:17 -0500

    Update schema to use correct references to other models

--- server/models/Game.js
+++ server/models/Game.js
@@ -1,10 +1,25 @@
-const { Schema, model } = require("mongoose");
-const User = require("./User");
-const Team = require("./Team");
-const Match = require("./Match");
+const { Schema, model } = require('mongoose');
+const User = require('./User');
+const Team = require('./Team');
+const Match = require('./Match');
 
+// > Game Schema
+// > - _id: String
+// > - name: String
+// > - description: String
+// > - teams: [Team]
+// > - matches: [Match]
+// > - maxPlayers: Number
+// > - minPlayers: Number
+// > - officialURL: String
+// > - rulesUrl: String
+// > - thumbnail: String
 // This is a subdocument schema, it won't become its own model but we'll use it as the schema for the User's `savedBooks` array in User.js
 const gameSchema = new Schema({
+	game_id: {
+		type: String,
+		required: true
+	},
 	name: {
 		type: String,
 		required: true,
@@ -12,7 +27,7 @@ const gameSchema = new Schema({
 	matches: [
 		{
 			type: Schema.Types.ObjectId,
-			ref: "Match",
+			ref: 'Match',
 		},
 	],
 	description: {
@@ -22,11 +37,11 @@ const gameSchema = new Schema({
 	teams: [
 		{
 			type: Schema.Types.ObjectId,
-			ref: "Team",
+			ref: 'Team',
 		},
 	],
 });
 
-const Game = model("Game", gameSchema);
+const Game = model('Game', gameSchema);
 
 module.exports = Team;
