6b09de29c9856197d6bbc47a95827362abd733a5
Ben S <benslinde@gmail.com>
2021-11-27 15:42:26 -0500

    altrulesbackend WIP

--- client/src/App.js
+++ client/src/App.js
@@ -3,41 +3,63 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Homepage from "./components/Homepage";
 import SearchGames from "./pages/SearchGames";
-import GameSearch from "./components/GameSearch";
+
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "./Theme/Theme";
-import SubmitBtn from "./components/SubmitBtn";
+
 import PieChartDemo from "./pages/PieChartDemo";
 import FunFact from "./pages/FactArray";
 import AltRules from "./pages/AltRules";
 
-// import {
-//   ApolloClient,
-//   InMemoryCache,
-//   ApolloProvider,
-//   createHttpLink,
-// } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 
-// Construct our main GraphQL API endpoint
-// const httpLink = createHttpLink({
-//   uri: '/graphql',
-// });
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+
+//Construct our main GraphQL API endpoint
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+// Construct request middleware that will attach the JWT token to every request as an `authorization` header
+const authLink = setContext((_, { headers }) => {
+	// get the authentication token from local storage if it exists
+	const token = localStorage.getItem('id_token');
+	// return the headers to the context so httpLink can read them
+	return {
+	  headers: {
+		...headers,
+		authorization: token ? `Bearer ${token}` : '',
+	  },
+	};
+  });
+
+const client = new ApolloClient({
+	// Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
+	link: authLink.concat(httpLink),
+	cache: new InMemoryCache(),
+});
+
 
 function App() {
 	return (
-		<div className="App">
-			<ThemeProvider theme={theme}>
-				<Navbar />
-				<FunFact />
-				<Homepage/>
-				<AltRules />
-				<PieChartDemo />
-				<SearchGames />
-				<GameSearch/>
-				<SubmitBtn />
-				<Footer />
-			</ThemeProvider>
-		</div>
+		<ApolloProvider client={client}>
+			
+				<ThemeProvider theme={theme}>
+					<Navbar />
+					<FunFact />
+					<Homepage/>
+					<AltRules />
+					<PieChartDemo />
+					<SearchGames />
+					<Footer />
+				</ThemeProvider>
+			
+		</ApolloProvider>	
 	);
 }
 
