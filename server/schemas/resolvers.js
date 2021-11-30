2fcd8ba2f21d24b7a1f9cd5a2da98249cc43acb3
Luke Poirrier <lukepoirrier5@gmail.com>
2021-11-30 16:39:28 -0500

    Add line of spacing

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -55,6 +55,7 @@ const resolvers = {
     //     return updatedUser;
     //   }
 
+
     //   throw new AuthenticationError('You need to be logged in!');
     // },
     // removeBook: async (parent, { bookId }, context) => {
