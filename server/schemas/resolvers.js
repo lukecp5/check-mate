a8769ff61a4bd6d7dbb213c2019d2f447d1bcef6
Luke Poirrier <lukepoirrier5@gmail.com>
2021-11-30 17:02:11 -0500

    Getting ready to merge updates to main

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -53,16 +53,16 @@ const resolvers = {
       const token = signToken(user);
       return { token, user };
     },
-    // saveBook: async (parent, { bookData }, context) => {
-    //   if (context.user) {
-    //     const updatedUser = await User.findByIdAndUpdate(
-    //       { _id: context.user._id },
-    //       { $push: { savedBooks: bookData } },
-    //       { new: true }
-    //     );
+    saveWin: async (parent, { winData }, context) => {
+      if (context.user) {
+        const updatedUser = await User.findByIdAndUpdate(
+          { _id: context.user._id },
+          { $push: { wins: winData } },
+          { new: true }
+        );
 
-    //     return updatedUser;
-    //   }
+        return updatedUser;
+      }
 
 
     //   throw new AuthenticationError('You need to be logged in!');
