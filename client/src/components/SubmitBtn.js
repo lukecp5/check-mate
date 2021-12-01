7200027eee4a2c5cb0c3fde8599b1d4cf59b4c7b
Amanda Morgan <AmandaC0022@gmail.com>
2021-12-01 15:28:06 -0600

    styled login page and added welcome message

--- client/src/components/SubmitBtn.js
+++ client/src/components/SubmitBtn.js
@@ -4,7 +4,7 @@ import { styled } from '@mui/system';
 const SubmitBtn = styled(Button)(({ theme }) => ({
     display: 'block', 
     height: 40,
-    background: `linear-gradient(to right, ${theme.palette.tertiary.main}, ${theme.palette.tertiary.light})`, 
+    background: `linear-gradient(to right, ${theme.palette.tertiary.dark}, ${theme.palette.tertiary.main}, ${theme.palette.tertiary.light})`, 
     color: 'white', 
     margin: 20, 
     fontSize: 20, 
