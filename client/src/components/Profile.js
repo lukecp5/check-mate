b5dca4c40c5d28f9565d88ad8d060a3aaf0a48ac
Amanda Morgan <AmandaC0022@gmail.com>
2021-11-30 21:01:31 -0600

    hard coded the statsBox and working on styling the Friendbox

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,42 +1,51 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import Card from '@mui/material/Card';
 // import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import CardActions from '@mui/material/CardActions';
 import { styled } from '@mui/system';
 import  { Grid }  from '@mui/material';
 import Avatar from '@mui/material/Avatar';
 import Stack from '@mui/material/Stack';
+import Pagination from '@mui/material/Pagination';
+import usePagination from '@mui/material/usePagination';
 
-// // STYLED Button 
-// const StyledButton = styled(Button)(({ theme }) => ({ 
-//     color: '#616161',
-//     padding: '5px',
-//     "&:hover": {
-//         color: '#ffffff',
-//         background: 'transparent',
-//         border: "2px solid white",
-//         padding: 'calc(5px -2px)',
-//     }
-// })); 
+const StatBox = styled(Card)(({ theme }) => ({
+    color: "#ffffff",
+    mt: '20px', 
+    padding: '10px', 
+    textAlign:'center', 
+    background: `linear-gradient(to right, ${theme.palette.tertiary.dark}, ${theme.palette.tertiary.main}, ${theme.palette.tertiary.light})`, 
+    marginBottom: '20px', 
+    textAlign: "left", 
+    height: 350
+})); 
 
-//This changes the colors of the backgrounds of each of the cards
-    // Theme colors added to array
-    var colors = ['#00A1CB','#01A4A4','#113F8C','#61AE24','#D0D102','#32742C','#E54028','#F18D05','#D70060'];
-    var randomColor = () => {
-        return colors[Math.floor(Math.random()* colors.length)];
-    };
-    var elements = document.getElementsByClassName(Card);
-        for (var i=0; i<elements.length; i++) {
-        elements[i].style.backgroundColor = randomColor();
-};
+const FriendBox = styled(Card)(({ theme }) => ({
+    color: "#ffffff",
+    mt: '20px', 
+    padding: '10px', 
+    textAlign:'center', 
+    background: `linear-gradient(to right, ${theme.palette.secondary.dark}, ${theme.palette.secondary.main}, ${theme.palette.secondary.light})`, 
+    marginBottom: '20px', 
+    textAlign: "left", 
+    height: 350
+})); 
 
 export default function Profile() {
+    const [page, setPage] = useState(1); 
+
+    const handlePageChange = (event, newPage) => {
+        setPage(newPage); 
+        console.log(event.target); 
+    }
+
     return (
-    <Grid>
-        <Grid sx={{}}>
-            <Card sx={{ color: "#ffffff", mt: '10px', padding: '10px', textAlign:'center', background: `linear-gradient(to right, #D70060, #F18D05)`, marginBottom: '20px', textAlign: 'left'}}>
+    <Grid container spacing={2}>
+        <Grid item xs={12} sm={6} md={5}>
+            <StatBox>
                 <CardContent >
                     <Stack direction="row" spacing={2}>
                         <Avatar alt="User" src="/static/images/avatar/1.jpg" />
@@ -44,56 +53,45 @@ export default function Profile() {
                             Welcome back, User!
                         </Typography>
                     </Stack>
+                    <Stack direction="column" spacing={2}>
+                        <Typography variant="body1" component="div" sx={{mt: '10px'}}>
+                            Wins: 0 
+                        </Typography>
+                        <Typography variant="body1" component="div">
+                            Losses: 0 
+                        </Typography>
+                        <Typography variant="body1" component="div">
+                            Best Teammate: Chris 
+                        </Typography>
+                        <Typography variant="body1" component="div">
+                            Arch Nemesis: Ben 
+                        </Typography>
+                        <Typography variant="body1" component="div">
+                            Favorite Game: Monopoly  
+                        </Typography>
+                    </Stack>
                 </CardContent>
-            </Card>
+            </StatBox>
         </Grid>
-        <Grid container sx={{width: '100%', justifyContent: 'space-evenly'}}>
-            <Grid item xs={12} sm={6} md={4} lg={3} xl={2} >
-                <CardContent sx={{ textAlign: 'center'}}>
-                        <Card sx={{ minWidth: 300, maxWidth: 600, minHeight: 400, margin: '20px', color: "#ffffff", background: randomColor, padding: '10px', }}>
-                            <CardContent sx={{ textAlign: 'center'}}>
-                                <Typography variant="h4" gutterBottom component="div">
-                                    Stats
-                                </Typography>
-                            </CardContent>
-                    </Card>
+        <Grid item xs={12} sm={6} md={7}>
+            <FriendBox>
+                <CardContent>
+                    <Grid container spacing={2}> 
+                    {Array.from(Array(6)).map((_, index) => (
+                        <Grid item xs={3} sm={4} md={4} key={index}>
+                            <Avatar>B</Avatar>
+                            <Typography xs={{textAlign:"center"}} varient="h6">Ben</Typography>
+                        </Grid>
+                        ))}
+                    </Grid>
+                    <Stack spacing={1} xs={{ width: "100%"}}>
+                        <Pagination count={5} variant="outlined" page={page} onPageChange={handlePageChange}/> 
+                    </Stack>
                 </CardContent>
-            </Grid>
-            <Grid item xs={12} sm={6} md={4} lg={3} xl={2} >
-                <CardContent sx={{ textAlign: 'center'}}>
-                        <Card sx={{ minWidth: 300, maxWidth: 600, minHeight: 400, margin: '20px', color: "#ffffff", background: randomColor, padding: '10px', }}>
-                            <CardContent sx={{ textAlign: 'center'}}>
-                                <Typography variant="h4" gutterBottom component="div">
-                                    Reviews
-                                </Typography>
-                            </CardContent>
-                    </Card>
-                </CardContent>
-            </Grid>
-            <Grid item xs={12} sm={6} md={4} lg={3} xl={2} >
-                <CardContent sx={{ textAlign: 'center'}}>
-                        <Card sx={{ minWidth: 300, maxWidth: 600, minHeight: 400, margin: '20px', color: "#ffffff", background: randomColor, padding: '10px', }}>
-                            <CardContent sx={{ textAlign: 'center'}}>
-                                <Typography variant="h4" gutterBottom component="div">
-                                    Friends
-                                </Typography>
-                            </CardContent>
-                    </Card>
-                </CardContent>
-            </Grid>
-            <Grid item xs={12} sm={6} md={4} lg={3} xl={2} >
-                <CardContent sx={{ textAlign: 'center'}}>
-                        <Card sx={{ minWidth: 300, maxWidth: 600, minHeight: 400, margin: '20px', color: "#ffffff", background: randomColor, padding: '10px', }}>
-                            <CardContent sx={{ textAlign: 'center'}}>
-                                <Typography variant="h4" gutterBottom component="div">
-                                    Alternate Rules
-                                </Typography>
-                            </CardContent>
-                    </Card>
-                </CardContent>
-            </Grid>
+            </FriendBox>
         </Grid>
     </Grid>
+
 
     );
 }
