72b885c5e189d851b4796ba2d86dde43c9db93b0
Luke Poirrier <lukepoirrier5@gmail.com>
2021-11-30 19:44:59 -0500

    Remove unnecessary imports from models index.js

--- server/models/index.js
+++ server/models/index.js
@@ -1,7 +1,4 @@
 const User = require("./User");
-const Match = require("./Match");
-const Game = require("./Game");
-const Team = require("./Team");
 const Altrules = require("./Altrules");
 
-module.exports = { User, Match, Game, Team, Altrules };
+module.exports = { User, Altrules };
