a5a9887465c296d411605829c547abefe7ce1493
Amanda Morgan <AmandaC0022@gmail.com>
2021-12-02 20:54:40 -0600

    added Results.js page along with functionality

--- client/src/pages/Play.js
+++ client/src/pages/Play.js
@@ -2,6 +2,9 @@ import React from 'react'
 import Autocomplete from '@mui/material/Autocomplete';
 import TextField from '@mui/material/TextField';
 import Stack from '@mui/material/Stack';
+import Typography from '@mui/material/Typography';
+import SubmitBtn from '../components/SubmitBtn';
+import { Link } from 'react-router-dom';
 
 const Play = () => {
     // const [friends, setFriends] = useState(" "); 
@@ -15,22 +18,26 @@ const Play = () => {
     ]
 
     return (
-        <Stack spacing={3} sx={{ width: 500 }}>
-        <Autocomplete
-        multiple
-        id="tags-outlined"
-        options={friends}
-        getOptionLabel={(option) => option.name}
-        // defaultValue={friends[0].name}
-        filterSelectedOptions
-        renderInput={(params) => (
-          <TextField
-            {...params}
-            label="Choose Players"
-            placeholder="Select Players"
-          />
-        )}
-      />
+        <Stack spacing={3} 
+          sx={{display: 'flex', alignContent: 'center', flexWrap: 'wrap', textAlign: 'center', flexDirection: 'column'}}>
+          <Typography variant="h3">Game Name</Typography>
+          <Typography variant="h5">Choose Your Players</Typography>
+          <Autocomplete
+          multiple
+          sx={{ width: 300 }}
+          id="tags-outlined"
+          options={friends}
+          getOptionLabel={(option) => option.name}
+          filterSelectedOptions
+          renderInput={(params) => (
+            <TextField
+              {...params}
+              label="Choose Players"
+              placeholder="Select Players"
+            />
+          )}
+        />
+        <SubmitBtn><Link to="/results">Ok!</Link></SubmitBtn>
       </Stack>
     )
 }
