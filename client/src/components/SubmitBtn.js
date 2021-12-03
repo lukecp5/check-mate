1308c576441619894965988c9440e416b4dfee88
Amanda Morgan <AmandaC0022@gmail.com>
2021-12-03 09:52:16 -0600

    added height of auto to SubmitBtn so that the text is central

--- client/src/components/SubmitBtn.js
+++ client/src/components/SubmitBtn.js
@@ -3,7 +3,7 @@ import { styled } from '@mui/system';
 
 const SubmitBtn = styled(Button)(({ theme }) => ({
     display: 'block', 
-    height: 40,
+    height: "auto",
     background: `linear-gradient(to right, ${theme.palette.tertiary.dark}, ${theme.palette.tertiary.main}, ${theme.palette.tertiary.light})`, 
     color: 'white', 
     margin: 20, 
