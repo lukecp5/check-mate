1397f69cf009c543c787ef2dec7390217e9b7908
hannahnmcdonald <hannahnmcdonald@protonmail.com>
2021-12-04 20:04:26 -0500

    alternate rules changed to alt rules on tabs for game due to mobile responsive issues

--- client/src/pages/SearchGames.js
+++ client/src/pages/SearchGames.js
@@ -268,7 +268,7 @@ const SearchGames = () => {
             <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
               <Tab label="About" {...a11yProps(0)} />
               <Tab label="Official Rules" {...a11yProps(1)} />
-              <Tab label="Alternate Rules" {...a11yProps(2)} />
+              <Tab label="Alt Rules" {...a11yProps(2)} />
             </Tabs>
           </Box>
           <TabPanel value={value} index={0}>
