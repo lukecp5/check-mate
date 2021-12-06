aa6cb2aa9a8d5f47c60b91724da02e0906760fea
Luke Poirrier <lukepoirrier5@gmail.com>
2021-12-06 15:41:12 -0500

    Create new ADD_LOSS mutation on frontend and delete old ADD_WIN mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -50,50 +50,21 @@ export const ADD_WIN = gql`
     }
   }`;
 
-// export const ADD_WIN = gql`
-//   mutation addWin(
-//   $firstName: String!
-//   $wins: WinInput
-//   ) {
-//     addWin(
-//       firstName: $firstName
-//       wins: $WinInput
-//       ) {
-//       firstName
-//       wins{
-//         game
-//         wins      
-//       }
-//     }
-//   }`;
-
-
 export const ADD_LOSS = gql`
   mutation addLoss(
-  $game: String!
+    $firstName: String!
+    $game: String!
   ) {
-    addLoss(
-      game: $game) {
-        _id
-        firstName
-        lastName
-        email
-        wins{
-          game
-          wins
-        }
-        losses{
-          game
-          losses
-        }
-        ties{
-          game
-          ties
-        }
-
+    addLoss(firstName: $firstName, game: $game) {
+      _id
+      firstName
+      lastName
+      losses{
+        game
+        wins
+      }
     }
-  }
-  `;
+  }`;
 
   export const ADD_TIE = gql`
   mutation addTie(
